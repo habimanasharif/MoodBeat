86b9ddd860390365b8115943c6c4c0a0d9f7f166
sharif Habiman <hdidiersharif@gmail.com>
2023-06-06 20:53:25 +0200

    designing mobile design

--- components/layout/Favorite.tsx
+++ components/layout/Favorite.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import FavouriteCard from './FavouriteCard';
 
 const Favorite = () => (
-  <div className="py-3">
+  <div className="py-3  h-[60%]">
     <h1>
       Recent Favourite
     </h1>
-    <div className="flex w-full overflow-x-hidden flex-grow overflow-y-auto ">
+    <div className=" w-full h-full  overflow-y-auto mt-3 ">
       <div className=" flex flex-wrap w-full ">
+        <FavouriteCard />
+        <FavouriteCard />
+        <FavouriteCard />
+        <FavouriteCard />
+        <FavouriteCard />
         <FavouriteCard />
         <FavouriteCard />
         <FavouriteCard />
--- components/layout/FavouriteCard.tsx
+++ components/layout/FavouriteCard.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import two from '../../public/two.jpg';
 
 const FavouriteCard = () => (
-  <div className="h-[11rem] sm:w-[12rem] w-[10.6rem] mt-7 sm:pr-6 ">
-    <div className=" w-[100%] h-[80%] sm:mr-3 mr-1 relative">
+  <div className="h-[11rem] sm:w-[8rem] w-[10.6rem] sm:pr-6 ">
+    <div className=" w-[100%] sm:h-[60%] h-[80%] sm:mr-3 mr-1 relative">
       <div className="w-[100%]  h-full">
         <div className="w-[100%] h-[100%] rounded-[1.2rem]  ">
           <div>
@@ -19,7 +19,7 @@ const FavouriteCard = () => (
         </div>
       </div>
     </div>
-    <div className="favourite-info text-white sm:text-dark">
+    <div className="favourite-info text-white sm:text-[#000]">
       <div className="font-bold">Million</div>
       <div className="font-thin text-xs">Always neve</div>
     </div>
